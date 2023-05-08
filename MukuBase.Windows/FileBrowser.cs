2bdff6cf4ba5e3ef5b5d412165490d2f079e5d10
Bán Vince <mukunya054@gmail.com>
2023-05-08 18:14:10 +0200

    Namespace fix

--- MukuBase.Windows/FileBrowser.cs
+++ MukuBase.Windows/FileBrowser.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.IO;
 using Microsoft.Win32;
 
-namespace VBase
+namespace MukuBase.Windows
 {
     public class FileBrowser
     {				
--- MukuBase.Windows/RelayCommands.cs
+++ MukuBase.Windows/RelayCommands.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 using System.Windows.Input;
 
-namespace VBase
+namespace MukuBase.Windows
 {
     public class RelayCommand : ICommand
     {
